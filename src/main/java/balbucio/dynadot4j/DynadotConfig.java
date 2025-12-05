3bc76660ccd5587017446ef3c0c09e3b880c00a7
SrBalbucio <srbalbuciogf@gmail.com>
2025-12-05 01:00:59 -0300

    Adicionado config e adicionado licença

---
+++ src/main/java/balbucio/dynadot4j/DynadotConfig.java
@@ -0,0 +1,14 @@
+package balbucio.dynadot4j;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class DynadotConfig {
+
+    private String apiKey;
+
+}
